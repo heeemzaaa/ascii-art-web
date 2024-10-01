3cc7a325794bb8930939202a1f2a0f7678f0f25e
heeemzaaa <hamzaelkhawlani00@gmail.com>
2024-10-01 09:41:10 +0100

    ascii-art-web

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -19,95 +19,73 @@ func Read_file(s string) []string {
 	return ret
 }
 
-// counts how many new lines to print
+func Middle(slice []string) bool {
+	before := false
+	after := false
+	middle := true
+	i := 0
+	for ; i < len(slice); i++ {
+		if slice[i] == "" {
+			break
+		}
+	}
+	for j := i - 1; j >= 0; j++ {
+		before = false
+		if slice[j] != "" {
+			before = true
+			break
+		}
+	}
 
-func Count_next_line(line string) []int {
-	var ret []int
-	result := []int{}
-	j := 0
-	ret = append(ret, 0)
-	for i := 0; i < len(line); i++ {
-		if i+1 < len(line) && line[i] == '\\' {
-			if line[i+1] == 'r' {
-				fmt.Println("ok")
-				ret[j]++
-			}
-			i += 3
-			if i+1 < len(line) && line[i+1] != '\\' {
-				ret = append(ret, 0)
-				j++
+	for k := i + 1; k < len(slice); k++ {
+		before = false
+		if slice[k] != "" {
+			after = true
+			break
+		}
+	}
+	if before && after {
+		middle = true
+	} else {
+		middle = false
+	}
+	return middle
+}
+
+func CleanSlice(slice []string) []string {
+	check := true
+	for i := 0; i < len(slice); i++ {
+		if slice[i] != "" {
+			check = false
+		} else if slice[i] == "" {
+			if Middle(slice) && !check {
+				check = true
+				slice = append(slice[:i], slice[i+1:]...)
+			} else {
+				i++
 			}
 		}
 	}
-	if line[0] != '\\' && (1 < len(line) && line[1] != 'r') {
-		result = append(result, 0)
-	}
-	for i := 0; i < len(ret); i++ {
-		result = append(result, ret[i])
-	}
-	return result
+	return slice
 }
 
-// a function that prints ascii charachters
-
-func Print_art(file []string, splitted_line []string, lines_count []int) string {
-	holder := 0
+func PrintAscii(slice []string, file []string) string {
 	result := ""
-	i := 0
-	for ; i < len(splitted_line); i++ {
-		for ; (i == 0 && len(lines_count) > 1) && lines_count[i] > 0; lines_count[i]-- {
-			result += "\n"
-		}
-		for ; (i < len(lines_count) && len(lines_count) > 0) && lines_count[i] > 1; lines_count[i]-- {
-			result += "\n"
-		}
-		for j := 0; j < 8; j++ {
-			for k := 0; k < len(splitted_line[i]); k++ {
-				holder = (int(splitted_line[i][k])-32)*9 + j
-				result += file[holder]
+	holder := 0
+	for i := 0; i < len(slice); i++ {
+		if slice[i] == "" {
+			result += "\r\n"
+		} else {
+			for j := 0; j < 8; j++ {
+				for k := 0; k < len(slice[i]); k++ {
+					holder = (int(slice[i][k])-32)*9 + j
+					result += file[holder]
+				}
+				result += "\n"
 			}
-			result += "\n"
-		}
-	}
-	i = len(lines_count) - 1
-	if i >= 0 {
-		for ; lines_count[i] > 0 && len(lines_count) > 0; lines_count[i]-- {
-			result += "\n"
 		}
 	}
 	return result
-}
-
-func Check_if_empty(s []string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] != "" {
-			return true
-		}
-	}
-	return false
-}
-
-// clean splitted line from empty strings
-func Cleaned_split(s []string, lines_count []int) ([]string, []int) {
-	var ret []string
-	i := 0
-	j := 0
-	if s[0] == "" {
-		if !Check_if_empty(s) {
-			i++
-		}
-		for ; (i < len(s) && s[i] == "") && j > 0; i++ {
-			j = lines_count[i]
-			ret = append(ret, "")
-			j--
-		}
-	}
-	for ; i < len(s); i++ {
-		if s[i] != "" {
-			ret = append(ret, s[i])
-		}
-	}
-	return ret, lines_count
 }
 
 func Is_ascii(s string) string {
@@ -137,14 +115,12 @@ func FinalPrint(text string, banner string) string {
 	}
 	line := text
 	ret := Is_ascii(line)
-
 	if len(line) < 1 {
 		return ""
 	}
 	finalResult := ""
-	lines_count := Count_next_line(ret)
 	splitted_line := strings.Split(ret, "\r\n")
-	splitted_line, lines_count = Cleaned_split(splitted_line, lines_count)
-	finalResult = Print_art(file[1:], splitted_line, lines_count)
+	cleaned := CleanSlice(splitted_line)
+	finalResult = PrintAscii(cleaned, file[1:])
 	return finalResult
 }
